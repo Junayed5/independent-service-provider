c1ea7e6a68806a6140dcedc6b2b9de38777c755a
Junayed6 <abutahirjunayed@gmail.com>
2022-04-18 21:35:23 +0600

    Reset password

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import auth from '../../firebase.init';
 import Loading from '../Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
-    
+    const emailRef = useRef('')
     const navigate =useNavigate()
     const location = useLocation();
+    let errorHandle;
     const from = location.state?.from?.pathname || "/";
     const [
         signInWithEmailAndPassword,
@@ -17,16 +20,17 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
-    let errorHandle;
+    const [sendPasswordResetEmail, sending, error2] = useSendPasswordResetEmail(auth);
+    
 
     if (user) {
         navigate(from, { replace: true });
     }
-    if (loading) {
+    if (loading || sending) {
         return <Loading/>
     }
-    if (error) {
-        errorHandle = <p className='text-danger'>{error?.message}</p>
+    if (error || error2) {
+        errorHandle = <p className='text-danger'>{error?.message} {error2?.message}</p>
         navigate('/login');
         // console.log(error?.message);
     }
@@ -39,6 +43,16 @@ const Login = () => {
         signInWithEmailAndPassword(email,password)
     }
 
+    const handleResetPassword = async() => {
+        const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);   
+            toast('Sent email') 
+        }else{
+            toast('Please enter your email')
+        }
+        
+    }
 
     return (
         <div className='w-25 mx-auto'>
@@ -46,7 +60,7 @@ const Login = () => {
             <Form onSubmit={handleLoginSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control name='email' type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} name='email' type="email" placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -54,12 +68,14 @@ const Login = () => {
                     <Form.Control name='password' type="password" placeholder="Password" />
                 </Form.Group>
                 <p>New to Junayed's food? <Link className='text-decoration-none' to='/register'>Please Register</Link></p>
+                <p>Forget password? <button onClick={handleResetPassword} className='btn btn-link text-decoration-none'>Please reset</button></p>
                 {errorHandle}
                 <Button className='w-100 d-flex justify-content-center' variant="warning" type="submit">
                     Login
                 </Button>
             </Form>
             <SocialLogin></SocialLogin>
+            <ToastContainer />
         </div>
     );
 };
