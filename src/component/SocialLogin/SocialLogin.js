4e70b673a9dd4e49b2df6a6d0fe438590924d10b
Junayed6 <abutahirjunayed@gmail.com>
2022-04-18 15:54:25 +0600

    google authentication

--- src/component/SocialLogin/SocialLogin.js
+++ src/component/SocialLogin/SocialLogin.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import google from '../../images/google.png';
+import Loading from '../Loading/Loading';
 
 const SocialLogin = () => {
 
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    const navigate = useNavigate();
+    let loadError;
+
+    if (user) {
+        navigate('/login')
+    }
+
+    if (loading) {
+        return <Loading/>
+    }
+
+    if (error) {
+        loadError = <p>{error}</p>
+    }
+    
+    const handleSignInWithGoogle =() => {
+        signInWithGoogle()
+    }
     
     return (
         <div>
